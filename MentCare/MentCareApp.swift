1e1a77cfd6be55908e4f2f73a415529c7f9df8b3
byt23 <berkayyasar2004@gmail.com>
2026-05-14 22:19:36 +0300

    Initial Commit

---
+++ MentCare/MentCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentCareApp.swift
+//  MentCare
+//
+//  Created by BERKAY TURAN on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MentCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
